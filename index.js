f548fd93472b28088f0844373087e5b84613a00c
Kiril Vatev <vatev.1@gmail.com>
2015-06-19 21:49:46 -0400

    updating with supplementary files

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
 var through = require('through2');
 
 module.exports = function MultiDest() {
+    // get all streams from the args in an array
     var destinations = Array.prototype.slice.call(arguments);
+    // get an output stream
     var output = through.obj();
     
     function onData(data) {
+        // copy any data to all streams
         destinations.forEach(function(dest){
             dest.write(data);
         });
     }
     
     function onEnd() {
+        // end all streams
         destinations.forEach(function(dest){
             dest.end();
         });
