f58031eb89e0ea9b8c081a4b035a9224995bfce5
Kiril Vatev <vatev.1@gmail.com>
2017-09-18 20:20:22 -0400

    adding better tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 /* jshint node: true, mocha: true */
 var expect = require('chai').expect;
 var es = require('event-stream');
+var through = require('through2');
 var File = require('vinyl');
 var async = require('async');
 
@@ -8,6 +9,50 @@ var multistream = require('../');
 
 describe('[index]', function () {
     var fakeData = 'llama';
+
+    function slowStream(time) {
+        return through.obj(function (obj, enc, cb) {
+            setTimeout(function () {
+                cb(null, obj);
+            }, time || 1);
+        });
+    }
+
+    function wait(stream) {
+        return function (next) {
+            stream.pipe(es.wait(next));
+        };
+    }
+
+    function doMultiWrite(input, out1, out2) {
+        var multi = multistream(out1, out2);
+
+        setImmediate(function () {
+            multi.write(input);
+            multi.end();
+        });
+
+        return multi;
+    }
+
+    function test(input, out1, out2, done) {
+        var multi = doMultiWrite(input, out1, out2);
+        var data = [];
+
+        async.parallel([
+            wait(out1),
+            wait(out2),
+            wait(multi)
+        ], function (err) {
+            done(err, data);
+        });
+
+        multi.on('data', function (chunk) {
+            data.push(chunk);
+        });
+
+        return multi;
+    }
 
     it('works with stream data', function (done) {
         var out1 = es.through();
@@ -17,19 +62,71 @@ describe('[index]', function () {
             contents: es.readArray([fakeData])
         });
 
-        var multi = multistream(out1, out2);
+        test(inputFile, out1, out2, function (err, data) {
+            if (err) {
+                return done(err);
+            }
 
-        async.parallel([
-            function (next) { out1.on('end', next); },
-            function (next) { out2.on('end', next); },
-            function (next) { multi.on('end', next); }
-        ], done);
+            expect(data).to.have.lengthOf(1);
 
-        multi.write(inputFile);
-        multi.end();
+            var file = data[0];
+
+            expect(file).to.equal(inputFile);
+            expect(file.isBuffer()).to.equal(false);
+            expect(file.isStream()).to.equal(true);
+
+            done();
+        });
     });
 
-    it('works with buffer data');
+    it('works with buffer data', function (done) {
+        var out1 = es.through();
+        var out2 = es.through();
 
-    it('waits for all destination streams to finish before ending');
+        var inputFile = new File({ contents: new Buffer(fakeData) });
+
+        test(inputFile, out1, out2, function (err, data) {
+            if (err) {
+                return done(err);
+            }
+
+            expect(data).to.have.lengthOf(1);
+
+            var file = data[0];
+
+            expect(file).to.equal(inputFile);
+            expect(file.isBuffer()).to.equal(true);
+            expect(file.isStream()).to.equal(false);
+
+            done();
+        });
+    });
+
+    it('waits for all destination streams to finish before ending', function (done) {
+        var order = [];
+        var out1 = slowStream(1);
+        var out2 = slowStream(2);
+
+        function waitOrder(name, stream) {
+            return function (next) {
+                wait(stream)(function (err) {
+                    order.push(name);
+                    next(err);
+                });
+            };
+        }
+
+        var inputFile = new File({ contents: new Buffer(fakeData) });
+        var multi = doMultiWrite(inputFile, out1, out2);
+
+        async.parallel([
+            waitOrder('out1', out1),
+            waitOrder('out2', out2),
+            waitOrder('multi', multi)
+        ], function (err) {
+            expect(order).to.deep.equal(['out1', 'out2', 'multi']);
+            console.log(order);
+            done(err);
+        });
+    });
 });
